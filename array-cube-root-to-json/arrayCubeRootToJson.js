6684b3f99de2698157e002de3c31d07f1ac8f094
Kanika <>
2020-12-21 19:09:35 +0530

    code for cubeRoot

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,3 +1,28 @@
-const arrayCubeRootToJson = arr => {};
+const arrayCubeRootToJson = (arr) => {
+  let validateArray = Array.isArray(arr);
+  if (validateArray == false) {
+    throw "Please enter valid input";
+  }
+  let cubeRootArray = [];
+  let cubeObj = {};
+  arr.forEach((element) => {
+    cubeObj = {};
+    let value = Math.cbrt(element);
+    let elem = element.toString();
+    cubeObj = { elem: elem, value: value };
+    cubeRootArray.push(cubeObj);
+  });
+  cubeObj = {};
+  cubeRootArray.forEach((item) => (cubeObj[item.elem] = item.value));
+  let cubeRoot = JSON.stringify(cubeObj);
+  return cubeRoot;
+};
+try {
+  let arr = [false];
+  let result = arrayCubeRootToJson(arr);
+  console.log(result);
+} catch (err) {
+  console.log(err);
+}
 
-export { arrayCubeRootToJson };
+module.exports.arrayCubeRootToJson = arrayCubeRootToJson;
